09579615dc415ae30b9cdcfa270b9667a1639bb4
christine <chrisitne.brown@bhcc.edu>
2022-06-09 21:51:24 -0400

    add delete route handler for field and value

--- src/main/java/com/careerdevs/stockapi/controllers/OverviewController.java
+++ src/main/java/com/careerdevs/stockapi/controllers/OverviewController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,14 +30,14 @@ public class OverviewController {
     public ResponseEntity<?> testOverview(RestTemplate restTemplate) {
         try {
             String APIKEY = env.getProperty("api_key");
-            String url = BASE_URL + "&symbol=IBM&apikey=demo" ;
+            String url = BASE_URL + "&symbol=IBM&apikey=demo";
             Overview responseBody = restTemplate.getForObject(url, Overview.class);
             return ResponseEntity.ok(responseBody);
-        }catch(HttpClientErrorException e) {
+        } catch (HttpClientErrorException e) {
             return ApiError.customApiError(e.getMessage(), e.getStatusCode().value());
-        }catch(IllegalArgumentException e) {
-            return ApiError.customApiError("Uri is not absolute. check url",500);
-        }catch (Exception e) {
+        } catch (IllegalArgumentException e) {
+            return ApiError.customApiError("Uri is not absolute. check url", 500);
+        } catch (Exception e) {
             return ResponseEntity.internalServerError().body(e.getMessage());
         }
     }
@@ -49,88 +50,96 @@ public class OverviewController {
             System.out.println(url);
 
             Overview responseBody = restTemplate.getForObject(url, Overview.class);
-            if(responseBody == null){
-                ApiError.throwErr(500 , "did not receive a response" );
-            }else if(responseBody.getSymbol() == null){
+            if (responseBody == null) {
+                ApiError.throwErr(500, "did not receive a response");
+            } else if (responseBody.getSymbol() == null) {
                 return ApiError.customApiError("Invalid stock symbol " + symbol, 404);
             }
             return ResponseEntity.ok(responseBody);
-        } catch(HttpClientErrorException e) {
+        } catch (HttpClientErrorException e) {
             return ApiError.customApiError(e.getMessage(), e.getStatusCode().value());
-        }catch(DataIntegrityViolationException e) {
+        } catch (DataIntegrityViolationException e) {
             return ApiError.customApiError("can not upload duplicate Stock data", 400);
-        }catch(IllegalArgumentException e)
-        {
+        } catch (IllegalArgumentException e) {
             return ApiError.customApiError("uri is not absolute. Check url", 500);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.internalServerError().body(e.getMessage());
         }
     }
+
     @PostMapping("/test")
     public ResponseEntity<?> testUpload(RestTemplate restTemplate) {
         try {
-            String APIKEY = env.getProperty("api_key");
-            String url = BASE_URL + "&symbol=IBM&apikey=demo" ;
-            Overview responseBody = restTemplate.getForObject(url, Overview.class);
-            if(responseBody == null){
-                return ApiError.customApiError("did not receive a response" , 500);
-            }else if(responseBody.getSymbol() == null){
-                return ApiError.customApiError("no data retrieved", 404);//HttpStatus.NOT_FOUND );
+            String[] testSymbols = {"VIEW", "CCL", "MTN", "NXPI"};
+            ArrayList<Overview> overviews = new ArrayList<>();
+
+            for (int i = 0; i < testSymbols.length; i++) {
+                String symbol = testSymbols[i];
+                String APIKEY = env.getProperty("api_key");
+                String url = BASE_URL + "&symbol=" + symbol + "&apikey=" + APIKEY;
+                Overview responseBody = restTemplate.getForObject(url, Overview.class);
+                if (responseBody == null) {
+                    ApiError.throwErr(500, "did not receive a response");
+                } else if (responseBody.getSymbol() == null) {
+                    ApiError.throwErr(404, symbol + " no data retrieved");//HttpStatus.NOT_FOUND );
+                }
+                overviews.add(responseBody);
             }
-
-            Overview savedOverview = overviewRepository.save(responseBody);
-
+            Iterable<Overview> savedOverview = overviewRepository.saveAll(overviews);
             return ResponseEntity.ok(savedOverview);
-        }catch(HttpClientErrorException e) {
+
+        } catch (HttpClientErrorException e) {
             return ApiError.customApiError(e.getMessage(), e.getStatusCode().value());
-        }catch(DataIntegrityViolationException e) {
+        } catch (DataIntegrityViolationException e) {
             return ApiError.customApiError("can not upload duplicate Stock data", 400);
-        }catch(IllegalArgumentException e) {
+        } catch (IllegalArgumentException e) {
             return ApiError.customApiError("Uri is not absolute. check url", 500);
-        }catch (Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.internalServerError().body(e.getMessage());
         }
     }
+
     @PostMapping("/{symbol}")
     public ResponseEntity<?> uploadOverview(RestTemplate restTemplate,
-                                             @PathVariable("symbol") String symbol) {
+                                            @PathVariable("symbol") String symbol) {
         try {
             String url = BASE_URL + "&symbol=" + symbol + "&apikey=" + env.getProperty("api_key");
             System.out.println(url);
             Overview responseBody = restTemplate.getForObject(url, Overview.class);
-            if(responseBody == null){
-                return ApiError.customApiError("did not receive a response" , 500);//HttpStatus.INTERNAL_SERVER_ERROR);
-            }else if(responseBody.getSymbol() == null){
-                return ApiError.customApiError("Invalid stock symbol " + symbol, 404 );
+            if (responseBody == null) {
+                return ApiError.customApiError("did not receive a response", 500);//HttpStatus.INTERNAL_SERVER_ERROR);
+            } else if (responseBody.getSymbol() == null) {
+                return ApiError.customApiError("Invalid stock symbol " + symbol, 404);
             }
             Overview savedOverview = overviewRepository.save(responseBody);
 
             return ResponseEntity.ok(savedOverview);
-        } catch(HttpClientErrorException e) {
+        } catch (HttpClientErrorException e) {
             return ApiError.customApiError(e.getMessage(), e.getStatusCode().value());
-        }catch(DataIntegrityViolationException e) {
+        } catch (DataIntegrityViolationException e) {
             return ApiError.customApiError("can not upload duplicate Stock data", 400);
-        }catch(IllegalArgumentException e) {
+        } catch (IllegalArgumentException e) {
             return ApiError.customApiError("uri is not absolute. Check url", 500);//HttpStatus.INTERNAL_SERVER_ERROR
-        }catch (Exception e) {
+        } catch (Exception e) {
             return ApiError.genericApiError(e);
         }
     }
-            @GetMapping("/all")
-            private ResponseEntity<?>getAlOverviews(){
-                try{
-                    Iterable<Overview>allOverviews = overviewRepository.findAll();
-                    return ResponseEntity.ok(allOverviews);
 
-                }catch(HttpClientErrorException e) {
-                    return ApiError.customApiError(e.getMessage(), e.getStatusCode().value());
-                }catch(Exception e){
-                    return ApiError.genericApiError(e);
-                }
+    @GetMapping("/all")
+    private ResponseEntity<?> getAlOverviews() {
+        try {
+            Iterable<Overview> allOverviews = overviewRepository.findAll();
+            return ResponseEntity.ok(allOverviews);
+
+        } catch (HttpClientErrorException e) {
+            return ApiError.customApiError(e.getMessage(), e.getStatusCode().value());
+        } catch (Exception e) {
+            return ApiError.genericApiError(e);
+        }
     }
+
     @GetMapping("/id/{id}")
-    private ResponseEntity<?>getOverviewById(@PathVariable String id, String userId) {
+    private ResponseEntity<?> getOverviewById(@PathVariable String id, String userId) {
         try {
             //datatype that may or may not contain a certain type
             Optional<Overview> foundOverview = overviewRepository.findById(Long.parseLong(id));
@@ -139,16 +148,17 @@ public class OverviewController {
             }
             //Long userIdLong = Long.parseLong((id));
             return ResponseEntity.ok(foundOverview);
-        } catch(HttpClientErrorException e) {
-                return ApiError.customApiError(e.getMessage(), e.getStatusCode().value());
-        }catch (NumberFormatException e) {
+        } catch (HttpClientErrorException e) {
+            return ApiError.customApiError(e.getMessage(), e.getStatusCode().value());
+        } catch (NumberFormatException e) {
             return ApiError.customApiError("Invalid id must be a number", 400);//HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
             return ApiError.genericApiError(e);
         }
     }
+
     @GetMapping("/exchange/{exchange}")
-    private ResponseEntity<?>getOverviewBySymbol(@PathVariable String exchange) {
+    private ResponseEntity<?> getOverviewBySymbol(@PathVariable String exchange) {
         try {
             //datatype that may or may not contain a certain type
             List<Overview> foundOverview = overviewRepository.findByExchange(exchange);
@@ -157,14 +167,15 @@ public class OverviewController {
             }
             //Long userIdLong = Long.parseLong((id));
             return ResponseEntity.ok(foundOverview);
-        } catch(HttpClientErrorException e) {
+        } catch (HttpClientErrorException e) {
             return ApiError.customApiError(e.getMessage(), e.getStatusCode().value());
         } catch (Exception e) {
             return ApiError.genericApiError(e);
         }
     }
+
     @GetMapping("/assetType/{assetType}")
-    private ResponseEntity<?>getOverviewByAssetType(@PathVariable String assetType) {
+    private ResponseEntity<?> getOverviewByAssetType(@PathVariable String assetType) {
         try {
             //datatype that may or may not contain a certain type
             List<Overview> foundOverview = overviewRepository.findByAssetType(assetType);
@@ -173,39 +184,41 @@ public class OverviewController {
             }
             //Long userIdLong = Long.parseLong((id));
             return ResponseEntity.ok(foundOverview);
-        } catch(HttpClientErrorException e) {
+        } catch (HttpClientErrorException e) {
             return ApiError.customApiError(e.getMessage(), e.getStatusCode().value());
         } catch (Exception e) {
             return ApiError.genericApiError(e);
         }
     }
+
     @GetMapping("/sector/{sector}")
-    private ResponseEntity<?>getOverviewBySector(@PathVariable String sector) {
+    private ResponseEntity<?> getOverviewBySector(@PathVariable String sector) {
         try {
             //datatype that may or may not contain a certain type
             List<Overview> foundOverview = overviewRepository.findBySector(sector);
             if (foundOverview.isEmpty()) {
-                ApiError.throwErr(404, sector + " did not match any overview" );
+                ApiError.throwErr(404, sector + " did not match any overview");
             }
             //Long userIdLong = Long.parseLong((id));
             return ResponseEntity.ok(foundOverview);
-        } catch(HttpClientErrorException e) {
+        } catch (HttpClientErrorException e) {
             return ApiError.customApiError(e.getMessage(), e.getStatusCode().value());
         } catch (Exception e) {
             return ApiError.genericApiError(e);
         }
     }
+
     @GetMapping("/country/{country}")
-    private ResponseEntity<?>getOverviewByCurrency(@PathVariable String country) {
+    private ResponseEntity<?> getOverviewByCountry(@PathVariable String country) {
         try {
             //datatype that may or may not contain a certain type
             List<Overview> foundOverview = overviewRepository.findByCountry(country);
             if (foundOverview.isEmpty()) {
-                ApiError.throwErr(404, country + "did not match any overview" );
+                ApiError.throwErr(404, country + "did not match any overview");
             }
             //Long userIdLong = Long.parseLong((id));
             return ResponseEntity.ok(foundOverview);
-        } catch(HttpClientErrorException e) {
+        } catch (HttpClientErrorException e) {
             return ApiError.customApiError(e.getMessage(), e.getStatusCode().value());
         } catch (Exception e) {
             return ApiError.genericApiError(e);
@@ -213,36 +226,65 @@ public class OverviewController {
     }
 
     @DeleteMapping("/all")
-    private ResponseEntity<?>deleteAllOverviews(){
-        try{
+    private ResponseEntity<?> deleteAllOverviews() {
+        try {
             long allOverviewCount = overviewRepository.count();
-            if(allOverviewCount == 0)
+            if (allOverviewCount == 0)
                 return ResponseEntity.ok("No overview to delete");
             overviewRepository.deleteAll();
-            return ResponseEntity.ok("Deleted Overviews" + allOverviewCount);
+            return ResponseEntity.ok("Deleted Overviews " + allOverviewCount);
 
-        }catch(Exception e){
+        } catch (Exception e) {
             return ApiError.genericApiError(e);
         }
     }
+
     @DeleteMapping("/id/{id}")
-    private ResponseEntity<?> deleteAllOverviews(@PathVariable String id){
+    private ResponseEntity<?> deleteAllOverviews(@PathVariable String id) {
         try {
             long overviewId = Long.parseLong(id);
             //datatype that may or may not contain a certain type
             Optional<Overview> foundOverview = overviewRepository.findById(Long.parseLong(id));
-            if(foundOverview.isEmpty()){
-                return ApiError.customApiError(id + "did not match any overview" ,404);
+            if (foundOverview.isEmpty()) {
+                return ApiError.customApiError(id + "did not match any overview", 404);
             }
             overviewRepository.deleteById(overviewId);
             return ResponseEntity.ok(foundOverview);
-        } catch(HttpClientErrorException e) {
+        } catch (HttpClientErrorException e) {
             return ApiError.customApiError(e.getMessage(), e.getStatusCode().value());
-        }catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             return ApiError.customApiError("Invalid id must be a number", 400);//HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
             return ApiError.genericApiError(e);
         }
     }
 
+    @DeleteMapping("/{field}/{value}")
+    private ResponseEntity<?> deleteOverviewByField(@PathVariable String field, @PathVariable String value) {
+        try {
+            List<Overview> foundOverview = null;
+            System.out.println("Field: " + field);
+            System.out.println("Value: " + value);
+            field = field.toLowerCase();
+
+            switch (field) {
+                case "id" -> overviewRepository.deleteById(Long.parseLong(value));
+                case "symbol" -> foundOverview = overviewRepository.deleteBySymbol(value);
+                case "sector" -> foundOverview = overviewRepository.deleteBySector(value);
+                case "assetType" -> foundOverview = overviewRepository.deleteByAssetType(value);
+                case "currency" -> foundOverview = overviewRepository.deleteByCurrency(value);
+                case "country" -> foundOverview = overviewRepository.deleteByCountry(value);
+            }
+            if (foundOverview == null || foundOverview.isEmpty()) {
+                ApiError.throwErr(404, field + " did not match any overview");
+            }
+            return ResponseEntity.ok(foundOverview);
+        } catch (HttpClientErrorException e) {
+            return ApiError.customApiError(e.getMessage(), e.getStatusCode().value());
+        } catch (NumberFormatException e) {
+            return ApiError.customApiError("Invalid id must be a number", 400);//HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return ApiError.genericApiError(e);
+        }
+    }
 }
--- src/main/java/com/careerdevs/stockapi/repositories/OverviewRepository.java
+++ src/main/java/com/careerdevs/stockapi/repositories/OverviewRepository.java
@@ -1,17 +1,26 @@
 package com.careerdevs.stockapi.repositories;
 
 import com.careerdevs.stockapi.models.Overview;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
+@Transactional
 
-public interface OverviewRepository extends CrudRepository<Overview, Long > {
+public interface OverviewRepository extends JpaRepository<Overview, Long > {
    public Optional<Overview> findBySymbol(String symbol);
    public List<Overview> findByExchange(String exchange);
    public List<Overview> findByAssetType(String assetType);
    public List<Overview> findByCurrency(String currency);
    public List<Overview> findByCountry(String country);
    public List<Overview> findBySector(String sector);
+
+   public List<Overview> deleteBySymbol(String symbol);
+   public List<Overview> deleteByExchange(String exchange);
+   public List<Overview> deleteByAssetType(String assetType);
+   public List<Overview> deleteByCurrency(String currency);
+   public List<Overview> deleteByCountry(String country);
+   public List<Overview> deleteBySector(String sector);
 
 }
